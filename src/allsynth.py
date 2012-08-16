85577b5b2972fd10e0f14274af6cf72aec819d80
lgauthie <lgauthie@uvic.ca>
2012-08-16 00:54:16 -0700

    V2 of Polysynth. Now will a variable amount of voices. The pitch can now be any midi number.

--- src/allsynth.py
+++ src/allsynth.py
@@ -1,7 +1,9 @@
 from marsyas import *
 from marsyas_util import create
+from collections import deque
 
 from pygame.midi import *
+
 
 def main():
     init()
@@ -9,42 +11,7 @@ def main():
     read = Input.read
     midi = Input(0)
 
-    # This sets up a number list of oscillators, only the series needs
-    # to have a unique name for the series to work.
-    osc_type = "BlitOsc/b"
-    oscs = [["Series/osc%d" % i, [osc_type,"ADSR/adsr","Gain/gain"]] for i in range(44,52)]
-
-    # This turns all of our system specifications and stores them in
-    # a list. 
-    oscs = [create(net) for net in oscs]
-
-    osc_bank = ["Fanout/bank", oscs]
-    net = ["Series/fmnet", [osc_bank,"Sum/sum", "AudioSink/dest"]]
-
-    # Create network and intialize parameter mapping 
-    network = create(net)
-
-    # Output settings
-    sample_rate = 88200.0
-    network.updControl("mrs_real/israte", sample_rate)
-    network.updControl("AudioSink/dest/mrs_bool/initAudio", MarControlPtr.from_bool(True))
-
-    for osc in oscs:
-        osc.updControl(osc_type + "/mrs_bool/noteon", mbool(True))
-        osc.updControl("Gain/gain/mrs_real/gain", 0.6)
-
-    # Trigs
-    trigs = [(i + 44,
-              net.getControl("ADSR/adsr/mrs_bool/noteon"),
-              net.getControl("ADSR/adsr/mrs_bool/noteoff"),
-              net.getControl(osc_type + "/mrs_real/frequency")) \
-              for i, net in enumerate(oscs)]
-
-    maj = [72,74,76,77,79,81,83,84]
-    # set dem notes
-    for ((_, _, _, freq), i) in zip(trigs, maj):
-        freq.setValue_real(midi2freq(i))
-        print(midi2freq(i))
+    synth = Synth()
 
     while(True):
         if poll(midi):
@@ -53,27 +20,83 @@ def main():
             msg = read(midi, 1)
             note = msg[0][0][1]
             noteon = msg[0][0][0]
-            print(note)
-            if noteon == 144:
-                get_note_on(trigs, note).setValue_bool(mbool(True))
-                lastnote = note
-            elif noteon == 128:
-                get_note_off(trigs, note).setValue_bool(mbool(True))
-                lastnote = note
-        network.tick()
+            if noteon == 144 or noteon == 145:
+                synth.noteon(note)
+            elif noteon == 128 or noteon == 129:
+                synth.noteoff(note)
+        synth()
 
-def mbool(b):
-    return MarControlPtr.from_bool(b)
+class Synth:
+    def __init__(self, voices=8):
+        self.playing = deque()
+        self.off = deque([Osc() for _ in range(voices)])
 
-def get_note_on(trigs, num):
-    for (i, nton, _, _) in trigs:
-        if i == num:
-            return nton
+        osc_bank = ["Fanout/bank", [osc.get_network() for osc in self.off]]
+        net = ["Series/fmnet", [osc_bank,"Sum/sum", "AudioSink/dest"]]
 
-def get_note_off(trigs, num):
-    for (i, _, ntoff, _) in trigs:
-        if i == num:
-            return ntoff
+        # Create network and intialize parameter mapping 
+        self.network = create(net)
+
+        # Output settings
+        sample_rate = 88200.0
+        self.network.updControl("mrs_real/israte", sample_rate)
+        self.network.updControl("AudioSink/dest/mrs_bool/initAudio", MarControlPtr.from_bool(True))
+
+    def noteon(self, num):
+        if self.off:
+            osc = self.off.pop()
+            osc.noteon(num)
+            self.playing.appendleft(osc)
+        elif self.playing:
+            osc = self.playing.pop()
+            osc.noteon(num)
+            self.playing.appendleft(osc)
+
+    def noteoff(self, num):
+        for osc in self.playing:
+            if osc.num == num:
+                osc.noteoff()
+                self.off.append(osc)
+
+    def __call__(self):
+        self.network.tick()
+
+class Osc:
+    _num_of_oscs = 0
+
+    def __init__(self, osc_type="BlitOsc/bl"):
+        self.osc_type = osc_type
+        self.spec = lambda i: ["Series/osc%d" % i, [self.osc_type,"ADSR/adsr","Gain/gain"]]
+
+        self.osc = self._new_osc()
+
+        self.noteon_ctrl = self.osc.getControl("ADSR/adsr/mrs_bool/noteon")
+        self.noteoff_ctrl = self.osc.getControl("ADSR/adsr/mrs_bool/noteoff")
+        self.freq_ctrl = self.osc.getControl(osc_type + "/mrs_real/frequency")
+        self.osc.updControl(osc_type + "/mrs_bool/noteon", MarControlPtr.from_bool(True))
+
+        self.note = None
+        self.num = None
+
+    def _new_osc(self):
+        osc = create(self.spec(Osc._num_of_oscs))
+        Osc._num_of_oscs = Osc._num_of_oscs + 1
+        return osc
+
+    def noteon(self, num, pitch=None):
+        if pitch:
+            self.freq_ctrl.setValue_real(pitch)
+        else:
+            self.freq_ctrl.setValue_real(midi2freq(num))
+        self.num = num
+        self.noteon_ctrl.setValue_bool(True)
+
+    def noteoff(self):
+        self.num = None
+        self.noteoff_ctrl.setValue_bool(True)
+
+    def get_network(self):
+        return self.osc
 
 def midi2freq(num):
     return 440.0 * pow(2.0,((num-69)/12.0))
